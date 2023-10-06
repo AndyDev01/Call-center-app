f7a1a5918ce235a5c66845de94757b37375e05b7
andydev <andydev0101@gmail.com>
2023-10-06 20:32:55 +0300

    Initial commit

---
+++ src/icons/arrow-down.jsx
@@ -0,0 +1,20 @@
+export function ArrowDown({ className }) {
+  return (
+    <svg
+      className={className}
+      width="12"
+      height="7"
+      viewBox="0 0 12 7"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.31953 1.15979L5.99997 5.84023L10.6804 1.15979"
+        stroke="#727377"
+        strokeWidth="1.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
