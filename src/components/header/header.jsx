4ff3d4a64ca2cd555949882d95d568848bdc6cc0
AndyDev <andydev0101@gmail.com>
2023-12-26 01:51:07 +0300

    fix Header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between w-100% bg-[#F4F5F8] h-20 shadow-slate-300 shadow-md px-8">
       <h1 className="font text-[2rem] font-bold">
-        Call center app
+        Call-center app
       </h1>
       <div>
         git logo
